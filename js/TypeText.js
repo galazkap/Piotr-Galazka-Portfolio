00f3a03de88df3a1f03e2d33a72b443c52a48e7c
Piotr Gałązka <pgalazka98@gmail.com>
2020-12-10 17:16:48 +0100

    Updates

---
+++ js/TypeText.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type-text', {
+strings: [ 'a level designer', 'a programmer', 'a game developer'],
+typeSpeed: 80,
+backSpeed: 80,
+smartBackspace: true, // this is a default
+loop: true
+});
